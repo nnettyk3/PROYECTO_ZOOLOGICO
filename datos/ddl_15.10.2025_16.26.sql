839dd8455664e1701c392c3bc6bc2603b59fa7fe
nnettyk3 <katherinnepaineo655@gmail.com>
2025-10-19 19:02:03 +0000

    modified:   datos/ddl_15.10.2025_16.26.sql

--- datos/ddl_15.10.2025_16.26.sql
+++ datos/ddl_15.10.2025_16.26.sql
@@ -1,2 +1,40 @@
 
 
+INSERT INTO cuidadores (nombre, turno, especialidad) VALUES
+('Katalina Cortes', 'Mañana', 'Aves'),
+('Esteban Contreras', 'Tarde', 'Mamiferos'),
+('Cristina Montecinos', 'Noche', 'Reptiles'),
+('Luis Rojas', 'Mañana', 'Felinos'),
+('Laura Diaz', 'Tarde, Primates'),
+('Gabriel Fuentes', 'Noche', 'Peces'),
+('Sofia Vargas', 'Mañana', 'Insectos');
+
+INSERT INTO animal (especie, edad, habitat, id_cuidador) VALUES
+('Loro', 3, 'Aviario', 1),
+('Tigre', 5, 'Selva', 4),
+('Cocodrilo', 7, 'Pantano', 3),
+('Mono', 2, 'Bosque', 5),
+('Pez Payaso',1, 'Arrecife', 6),
+('Elefante', 10, 'Sabana', 2),
+('Mariposa', 0, 'Jardin', 7);
+
+INSERT INTO visitantes (nombre, rut) VALUES
+('Valeria Gonzales', 21.345.678-9),
+('Juaquin Perez', 19.887.679-5),
+('Andrea Salazar', 22.534,980-1),
+('Fernando Rivas', 19.544,221-4),
+('Camila Soto', 14.249.112-3),
+('Diego Moreno', 17.970.255-6),
+('Isabella Furntes', 20.123.096-0);
+
+INSERT INTO entrada (fecha_visita, precio, id_visitante) VALUES
+('01-10-2025', 5000, 1),
+('02-10-2025', 4500, 2),
+('03-10-2025', 6000, 3),
+('04-10-2025', 5500, 4),
+('05-10-2025', 7000, 5),
+('06-10-2025', 4800, 6),
+)
+
+
+
